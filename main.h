472ecde4baf48c31f3c2d269f79721bed36e397f
dayoonasanya <aonasanya@mail.dccc.edu>
2023-10-18 13:21:12 -0700

    I'm not going anywhere

--- main.h
+++ main.h
@@ -6,16 +6,16 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-int print_char(va_list args, int printed);
-int print_string(va_list args, int printed);
-int print_integer(va_list args, int printed);
-int print_octal(unsigned int num, int printed);
-int print_unsigned(unsigned int num, int printed);
-int print_reverse(va_list args, int printed);
-int print_pointer(va_list args, int printed);
-int select(const char *format, va_list args, int printed);
-int print_binary(unsigned int num, int printed);
+int print_char(va_list args, int p);
+int print_string(va_list args, int p);
+int print_integer(va_list args, int p);
+int print_octal(unsigned int n, int p);
+int print_unsigned(unsigned int n, int p);
+int print_reverse(va_list args, int p);
+int print_pointer(va_list args, int p);
+int select(const char *format, va_list args, int p);
+int print_binary(unsigned int n, int p);
 int _printf(const char *format, ...);
-int _xhex(unsigned int num, int printed, int uppercase);
+int _xhex(unsigned int n, int p, int uppercase);
 
 #endif

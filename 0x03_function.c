c76ff3507b4425daf0b35295995b54a95c2bdf11
dayoonasanya <aonasanya@mail.dccc.edu>
2023-10-20 08:59:22 -0700

    printf

---
+++ 0x03_function.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_rot13string - Prints a string in rot13.
+ * @types: List of arguments.
+ * @buffer: Buffer array for handling print.
+ * @flags: Calculates active flags.
+ * @width: Specifies the width for formatting.
+ * @precision: Precision specification for formatting.
+ * @size: Size specifier for formatting.
+ * Return: Number of characters printed.
+ */
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char x;
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(types, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		str = "(AHYY)";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				x = out[j];
+				write(1, &x, 1);
+				count++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			x = str[i];
+			write(1, &x, 1);
+			count++;
+		}
+	}
+	return (count);
+}
+
